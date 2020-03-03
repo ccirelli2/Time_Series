11272f293a5f9531cdc6aa4b0ddbb3b324f77646
Chris Cirelli <Chris.Cirelli@starrcompanies.com>
2020-03-03 06:27:21 -0500

    Update

---
+++ wk6/R_tutorial_time_series_forecasting_sea_surface_03032020.R
@@ -0,0 +1,4 @@
+' R Tutorial - Time Series Prediction - Sea Surface Temperatures
+  https://semba-blog.netlify.com/02/22/2019/exploring-time-series-data-in-r/
+
+'
---
+++ wk6/bayesian_time_series_forecasting_03032020.R
@@ -0,0 +1,9 @@
+'source: https://towardsdatascience.com/a-bayesian-approach-to-time-series-forecasting-d97dd4168cb7
+
+ Time Series Forecasting:  One of the issues is overfitting parameters. 
+ Author states that the solution to overfitting can be overcome using a Bayesian
+ approach as it imposes certain prior on our variables. 
+'
+
+
+
