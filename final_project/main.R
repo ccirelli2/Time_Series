7092e3f9bf75f791a15b450741ecc088144ddc44
cc2 <ccirelli2@gmail.com>
2020-04-10 08:53:45 -0400

    -q

---
+++ final_project/main.R
@@ -0,0 +1,7 @@
+## DOCUMENTATION __________________________________________________
+'''	Descr:	Main R script for project
+
+	Ref: https://www.analyticsvidhya.com/blog/2018/05/generate-accurate-forecasts-facebook-prophet-python-r/
+'''
+
+
