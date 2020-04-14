3ce12fed7e22327098378bb6473339aab6227f77
cc2 <ccirelli2@gmail.com>
2020-04-14 12:10:24 -0400

    -q

--- final_project/main.R
+++ final_project/main.R
@@ -1,7 +1,75 @@
 ## DOCUMENTATION __________________________________________________
-'''	Descr:	Main R script for project
-
+' Descr:	Main R script for project
+          Objective is to predict the count of Auto Thefts over time.
+          Time will be measured using the Occurance Date and Occurance Time
 	Ref: https://www.analyticsvidhya.com/blog/2018/05/generate-accurate-forecasts-facebook-prophet-python-r/
-'''
+'
+
+## Clear Namespace --------------------------------------------------
+rm(list=ls())
+
+## Load Libraries ---------------------------------------------------
+source('/home/cc2/Desktop/repositories/Time_Series/final_project/data/data_inspection.R')
+library(dplyr)
+library(ggplot2)
+
+## Load Data --------------------------------------------------------
+' data source:  http://opendataportal.azurewebsites.us/
+  description:  Atlanta Crime Data'
+data <- read.csv('/home/cc2/Desktop/repositories/Time_Series/final_project/data/data.csv')
+
+
+## Data Inspection --------------------------------------------------
+'  Observations:  Data set contains temporal, continuous, ordinar and categoricla data.
+                  Missing data:  Apartment Offic Prefix looks like it has missing values. 
+                  
+                  Target:        UCR.Literal
+'
+data_dict <- get_data_dict()
+col_names = names(data)
+col_names
+head(data)
+
+
+# Inspection Auto Thefts
+AutoTheft   <- data$UCR.Literal=='AUTO THEFT'
+data.at     <- data[AutoTheft, c('Occur.Date', 'Occur.Time', 'Beat', 'Shift.Occurence', 'Location.Type', 
+                        'Neighborhood', 'Latitude', 'Longitude')]
+summary(data.at)
+
+# Transform Columns 
+' Observations:  The minimum of the occurance date is 1916, which does not comport with the
+                 study which should have been from 2009 at the earliest
+  Null/Missing   Neighborhood = 1941
+
+'
+data.at$Occur.Date <- as.Date(data.at$Occur.Date, '%Y-%m-%d')
+data.at$Year       <- format(as.Date(data.at$Occur.Date, '%Y-%m-%d'), "%Y")
+data.at$Month      <- format(as.Date(data.at$Occur.Date, '%Y-%m-%d'), "%m")
+data.at$Day        <- format(as.Date(data.at$Occur.Date, '%Y-%m-%d'), "%d")
+data.at$Hour       <- substr(data.at$Occur.Time, 1,2)
+data.at$Count      <- rep(1, length(data.at$Year))
+data.at$Occur.Date <- NULL
+data.at$Occur.Time <- NULL
+col_names <- names(data.at)
+min.yr             <- data.at$Year > 2008
+data.at            <- data.at[min.yr, names(data.at)]
+min(data.at$Year)
+summary(data.at)
+
+# Plot Data
+yr.cnt    <- data.at %>% group_by(Year) %>% tally()
+month.cnt <- data.at %>% group_by(Month) %>% tally()
+day.cnt   <- data.at %>% group_by(Day) %>% tally()
+hour.cnt  <- data.at %>% group_by(Hour) %>% tally()
+plt.yr    <- ggplot(yr.cnt, aes(x=Year, y=n)) + geom_bar(stat='identity') + ggtitle("Count of Auto Thefts By Year")
+plt.month <- ggplot(month.cnt, aes(x=Month, y=n)) + geom_bar(stat='identity') + ggtitle("Count of Auto Thefts By Month")
+plt.day   <- ggplot(day.cnt, aes(x=Day, y=n)) + geom_bar(stat='identity') + ggtitle("Count of Auto Thefts By Day")
+plt.hour  <- ggplot(hour.cnt, aes(x=Hour, y=n)) + geom_bar(stat='identity') + ggtitle("Count of Auto Thefts By Hour")
+
+
+
+
+
 
 
--- hw4/hw4_q3.R
+++ hw4/hw4_q3.R
@@ -33,8 +33,19 @@ hist(Affairs$affairs)
 summary(Affairs)
 Affairs %>% group_by(gender) %>% summarise(affairs=sum(affairs))
 
+affairs.mu <- mean(Affairs$affairs)
+affairs.var <- var(Affairs$affairs)
+if affairs.var > affair.mu:
+  print('Variance is greater than mean')
+
 
 # Fit Poisson Model ----------------------------------
+' Results:
+    Residual Deviance:  2359 
+    AIC:                2871
+    Regressors:       Age, yearsmarried, religiousness, occupation and rating are all significant based
+                      on the p-value. 
+'
 m.poisson <- glm(affairs ~., data=Affairs, 
                  family=poisson(link='log'))
 
@@ -42,13 +53,30 @@ summary(m.poisson)
 
 
 # Fit Negative Binomial Regression -------------------
-m.nb <- glm.nb(affairs ~., data=Affairs)
+' Results:
+    Residual Deviance:  339 (substantially less than the poisson model)
+    AIC:                1476 (almost half the poisson model)
+    Log-likelihood:     -1456
+    Dispersion:         less than 1. 
+    Regressors:         only yearsmarried, religiousness and rating are significant based on p-value. 
+    Overall:            seems to be a better model based on the Residual erro and AIC scores and that the model
+                        does not assume mu = var. 
+'
 
+m.nb <- glm.nb(affairs ~., data=Affairs)
 summary(m.nb)
 
 # Fit Hurdle Model -----------------------------------
+' Results:
+    Model:            Fit both truncated poisson with log link and binomial with logit link. 
+    Log-likelihood:   -758.8
+    Overall:          Appears to be a better fit based on teh log-likelihood value. 
+    
+
+'
 m.hurdle.poisson <- hurdle(affairs~., data=Affairs, dist='poisson')
-m.hurdle.
+
+summary(m.hurdle.poisson)
 
 
 
